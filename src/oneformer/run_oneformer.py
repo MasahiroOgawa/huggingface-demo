a2dd41a5c4d74ebbdbbd78e836d02e4c76cf87ff
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-07-28 17:02:25 +0900

    extract label for each segment_ids.

--- src/oneformer/run_oneformer.py
+++ src/oneformer/run_oneformer.py
@@ -67,10 +67,8 @@ def run_segmentation(image, task_type="panoptic", model_name="shi-labs/oneformer
     print(f"predicted_map = {predicted_map}")
     print(f"segments_info = {segments_info}")
     for segment in segments_info:
-        print(f"segment id = {segment["id"]}")
-        print(f"segment label id = {segment["label_id"]}")
-        label = model.config.id2label[id['label_id']]
-        print(f"label = {label}")
+        label = model.config.id2label[segment['label_id']]
+        print(f"segment id = {segment["id"]} : {label}")
 
     return predicted_map
 
